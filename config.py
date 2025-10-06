930e9039c5a9597ee9cedf4d17ae99455e53fa84
BORA-Cyber <josebongue67@gmail.com>
2025-10-06 15:39:56 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# Credenciais do Telegram (API_ID e API_HASH)
+API_ID = 16626973 # SUBSTITUA pelo seu API_ID real
+API_HASH = '8b75ca97bee5ab5b4d7d8554283349ec' # SUBSTITUA pelo seu API_HASH real
+
+# O nome de usuário do seu grupo/canal (ex: '@meugrupo')
+# Se for uma ID, deve começar com -100...
+GRUPO_USERNAME = 'bro12gyv' # SUBSTITUA pelo username do seu grupo/canal
+
+# Nome do arquivo HTML do seu site que será atualizado
+HTML_FILE = 'index.html'
